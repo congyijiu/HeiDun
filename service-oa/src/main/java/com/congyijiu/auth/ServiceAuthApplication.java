dcef84af7da4e5f82b1f7e7bff77443e13c4244f
yijiu <2225668977@qq.com>
2023-05-30 22:45:19 +0800

    tyfytf

---
+++ service-oa/src/main/java/com/congyijiu/auth/ServiceAuthApplication.java
@@ -0,0 +1,21 @@
+package com.congyijiu.auth;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+/**
+ * @author congyijiu
+ * @create 2023-05-30-22:31
+ */
+
+@SpringBootApplication
+@ComponentScan("com.congyijiu")
+@MapperScan("com.atguigu.*.mapper")
+public class ServiceAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceAuthApplication.class, args);
+    }
+
+}
